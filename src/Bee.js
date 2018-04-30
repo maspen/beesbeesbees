e29e763bf4b6b199cbe7ac2ffce8d343325785e9
Matt Aspen <maspen@Matts-MacBook-Pro.local>
2018-04-30 15:14:04 -0700

    completed BeesBeesBees impl and all tests pass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+	Grub.call(this); // call Grup superclass
+	this.age = 5;
+	this.color = 'yellow';
+	//this.food; // TODO inherit from Grup
+	//this.eat; // TODO inherit from Grub - function
+	this.job = 'keep on growing';
 };
-
+Bee.prototype = Object.create(Grub.prototype); // sets inheritence
+Bee.prototype.constructor = Bee; // set Bee constructor function
+//Bee.prototype.food = Bee.prototype.food;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,18 @@
 var Grub = function() {
+	this.age = 0;
+	this.color = 'pink';
+	this.food = 'jelly';
 };
-
+Grub.prototype.eat = function() {
+  console.log('eating');
+};
+/*
+var Stack = function() {
+	this.storage = {}
+	this.length = 0;
+};
+Stack.prototype.push = function(value) {
+	this.storage[this.length] = value;
+	this.length++;
+}
+*/
