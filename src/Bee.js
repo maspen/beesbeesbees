907bc1cf49e37dca3fb84f3424cf2f9baeb39a62
Matt Aspen <mattaspen@gmail.com>
2018-06-17 17:00:12 -0700

    syncing up

--- src/Bee.js
+++ src/Bee.js
@@ -2,8 +2,6 @@ var Bee = function() {
 	Grub.call(this); // call Grup superclass
 	this.age = 5;
 	this.color = 'yellow';
-	//this.food; // TODO inherit from Grup
-	//this.eat; // TODO inherit from Grub - function
 	this.job = 'keep on growing';
 };
 Bee.prototype = Object.create(Grub.prototype); // sets inheritence
